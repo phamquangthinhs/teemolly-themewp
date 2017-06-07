e0e2da013eb6f30b78305d523f407d3a8b1141ba
Adam Baylin <vip.phamquangthinh@gmail.com>
2017-06-07 21:32:00 +0700

    news

--- templates/News.php
+++ templates/News.php
@@ -3,7 +3,7 @@
  Template Name: News
  */
  ?>
- <?php get_header(); ?>
+    <?php get_header(); ?>
     <div class="tm1 tm9">
         <ul>
             <li>
@@ -21,62 +21,72 @@
     <div class="tm10">
         <div class="container">
             <div class="row">
+                <?php 
+                  global $post;
+                  $args = array('numberposts'=>1,'category'=>26);
+                  $custom_posts = get_posts ($args);
+                  foreach($custom_posts as $post) : setup_postdata($post); ?>
                 <div class="col-lg-8 col-md-8 col-xs-12">
                     <div class="tn">
-                        <div class="tn1"><img src="<?php echo bloginfo('template_directory'); ?>/img/5.jpg" alt=""></div>
+                        <div class="tn1"><img src="<?php the_post_thumbnail(); ?>">
+                            <!--                        <?php echo bloginfo('template_directory'); ?>/img/5.jpg" alt="-->
+                        </div>
                         <div class="tn2">
                             <ul>
-                                <li><a href=".#">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet rutrum velit. Cras hendrerit, purus non porta tempus, nisi erat accumsan tellus, sed interdum arcu nunc non tellus.</a></li>
-                                <li>Lorem ipsum </li>
+                                <li>
+                                    <a href="<?php the_permalink(); ?>">
+                                        <?php the_title(); ?>
+                                    </a>
+                                </li>
+                                <li>
+                                    Adam Baylin
+                                </li>
                             </ul>
                         </div>
                     </div>
-
+                    <?php endforeach; wp_reset_postdata(); ?>
                     <div class="row">
+                        <?php 
+                  global $post;
+                  $args = array('numberposts'=>2,'category'=>27);
+                  $custom_posts = get_posts ($args);
+                  foreach($custom_posts as $post) : setup_postdata($post); ?>
                         <div class="col-md-6 col-xs-6">
                             <div class="tn tn3">
-                                <div class="tn1"><img src="<?php echo bloginfo('template_directory'); ?>/img/5.jpg" alt=""></div>
+                                <div class="tn1"><img src="<?php the_post_thumbnail(); ?>" alt=""></div>
                                 <div class="tn2">
                                     <ul>
-                                        <li><a href="#.">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet rutrum velit. Cras hendrerit, purus non porta tempus, nisi erat accumsan tellus, sed interdum arcu nunc non tellus.</a></li>
-                                        <li>Lorem ipsum </li>
+                                        <li>
+                                            <a href="<?php the_permalink(); ?>">
+                                                <?php the_title(); ?>
+                                            </a>
+                                        </li>
+                                        <li>Adam Baylin</li>
                                     </ul>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-6 col-xs-6">
-                            <div class="tn tn3">
-                                <div class="tn1"><img src="<?php echo bloginfo('template_directory'); ?>/img/5.jpg" alt=""></div>
-                                <div class="tn2">
-                                    <ul>
-                                        <li><a href="#.">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet rutrum velit. Cras hendrerit, purus non porta tempus, nisi erat accumsan tellus, sed interdum arcu nunc non tellus.</a></li>
-                                        <li>Lorem ipsum </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-
+                        <?php endforeach; wp_reset_postdata(); ?>
                     </div>
-
                 </div>
+                <!--                end tin chinh-->
                 <div class="col-lg-4 col-md-4 col-xs-10 col-lg-offset-0 col-md-offset-0 col-xs-offset-1">
                     <div class="tn4">
+                        <?php 
+                          global $post;
+                          $args = array('numberposts'=>4,'category'=>28);
+                          $custom_posts = get_posts ($args);
+                          foreach($custom_posts as $post) : setup_postdata($post); ?>
+
                         <ul>
-                            <li><a href="#.">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet rutrum velit. Cras hendrerit, purus non porta tempus, nisi erat accumsan tellus, sed interdum arcu nunc non tellus.</a></li>
-                            <li>Lorem ipsum </li>
+                            <li>
+                                <a href="<?php the_permalink(); ?>">
+                                    <?php the_title(); ?>
+                                </a>
+                            </li>
+                            <li>Adam Baylin</li>
                         </ul>
-                        <ul>
-                            <li><a href="#.">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet rutrum velit. Cras hendrerit, purus non porta tempus, nisi erat accumsan tellus, sed interdum arcu nunc non tellus.</a></li>
-                            <li>Lorem ipsum </li>
-                        </ul>
-                        <ul>
-                            <li><a href="#.">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet rutrum velit. Cras hendrerit, purus non porta tempus, nisi erat accumsan tellus, sed interdum arcu nunc non tellus.</a></li>
-                            <li>Lorem ipsum </li>
-                        </ul>
-                        <ul>
-                            <li><a href="#.">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet rutrum velit. Cras hendrerit, purus non porta tempus, nisi erat accumsan tellus, sed interdum arcu nunc non tellus.</a></li>
-                            <li>Lorem ipsum </li>
-                        </ul>
+                        <?php endforeach; wp_reset_postdata(); ?>
                     </div>
                 </div>
 
@@ -86,89 +96,54 @@
     <div class="tm11">
         <div class="container">
             <div class="row">
+                <?php 
+              global $post;
+              $args = array('numberposts'=>4,'category'=>29);
+              $custom_posts = get_posts ($args);
+              foreach($custom_posts as $post) : setup_postdata($post); ?>
+
                 <div class="col-lg-6 col-md-6 col-xs-12 tn2">
                     <div class="row">
                         <div class="col-md-6 col-xs-4">
-                            <div class="tn"><img src="<?php echo bloginfo('template_directory'); ?>/img/6.jpg" alt=""></div>
+                            <div class="tn"><img src="<?php the_post_thumbnail(); ?>" alt=""></div>
                         </div>
                         <div class="col-md-6 col-xs-8">
                             <div class="tn1">
                                 <ul>
-                                    <li><a href="#.">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet rutrum velit. Cras hendrerit, purus non</a></li>
-                                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet rutrum velit. Cras hendrerit, purus non porta tempus, nisi erat accumsan tellus, sed interdum arcu nunc non tellus.</li>
+                                    <li>
+                                        <a href="<?php the_permalink(); ?>">
+                                            <?php the_title(); ?>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <?php the_excerpt(); ?>
+                                    </li>
                                 </ul>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-6 col-md-6 col-xs-12 tn2">
-                    <div class="row">
-                        <div class="col-md-6 col-xs-4">
-                            <div class="tn"><img src="<?php echo bloginfo('template_directory'); ?>/img/6.jpg" alt=""></div>
-                        </div>
-                        <div class="col-md-6 col-xs-8">
-                            <div class="tn1">
-                                <ul>
-                                    <li><a href="#.">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet rutrum velit. Cras hendrerit, purus non</a></li>
-                                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet rutrum velit. Cras hendrerit, purus non porta tempus, nisi erat accumsan tellus, sed interdum arcu nunc non tellus.</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 col-md-6 col-xs-12 tn2">
-                    <div class="row">
-                        <div class="col-md-6 col-xs-4">
-                            <div class="tn"><img src="<?php echo bloginfo('template_directory'); ?>/img/6.jpg" alt=""></div>
-                        </div>
-                        <div class="col-md-6 col-xs-8">
-                            <div class="tn1">
-                                <ul>
-                                    <li><a href="#.">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet rutrum velit. Cras hendrerit, purus non</a></li>
-                                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet rutrum velit. Cras hendrerit, purus non porta tempus, nisi erat accumsan tellus, sed interdum arcu nunc non tellus.</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 col-md-6 col-xs-12 tn2">
-                    <div class="row">
-                        <div class="col-md-6 col-xs-4">
-                            <div class="tn"><img src="<?php echo bloginfo('template_directory'); ?>/img/6.jpg" alt=""></div>
-                        </div>
-                        <div class="col-md-6 col-xs-8">
-                            <div class="tn1">
-                                <ul>
-                                    <li><a href="#.">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet rutrum velit. Cras hendrerit, purus non</a></li>
-                                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet rutrum velit. Cras hendrerit, purus non porta tempus, nisi erat accumsan tellus, sed interdum arcu nunc non tellus.</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <?php endforeach; wp_reset_postdata();?>
+
             </div>
         </div>
     </div>
     <div class="tm12">
         <div class="container">
             <div class="row">
+                <?php 
+              global $post;
+              $args = array('numberposts'=>3,'category'=>30);
+              $custom_posts = get_posts ($args);
+              foreach($custom_posts as $post) : setup_postdata($post); ?>
                 <div class="col-lg-4 col-md-4 col-xs-10 col-lg-offset-0 col-md-offset-0 col-xs-offset-1 tn2">
                     <div class="tn1">
-                        <img src="<?php echo bloginfo('template_directory'); ?>/img/6.jpg" alt="">
+                        <img src="<?php the_post_thumbnail(); ?>" alt="">
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-4 col-xs-10 col-lg-offset-0 col-md-offset-0 col-xs-offset-1 tn2">
-                    <div class="tn1">
-                        <img src="<?php echo bloginfo('template_directory'); ?>/img/6.jpg" alt="">
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-4 col-xs-10 col-lg-offset-0 col-md-offset-0 col-xs-offset-1 tn2">
-                    <div class="tn1">
-                        <img src="<?php echo bloginfo('template_directory'); ?>/img/6.jpg" alt="">
-                    </div>
-                </div>
+                <?php endforeach; wp_reset_postdata(); ?>
             </div>
         </div>
     </div>
 
-   <?php get_footer(); ?>
+    <?php get_footer(); ?>
