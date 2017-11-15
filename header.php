f7e421bcafff7d84b4400c47ab7b3d48dd38c52d
Adam Baylin <vip.phamquangthinh@gmail.com>
2017-11-15 23:09:48 +0700

    update 15.11

--- header.php
+++ header.php
@@ -120,8 +120,8 @@
 
     <div class="tm">
         <div class="container">
-            <div class="row justify-content-sm-center">
-                <ul class="col-xl-5 col-lg-5 col-md-5 col-sm-auto">
+            <div class="row justify-content-sm-center justify-content-center">
+                <ul class="col-xl-5 col-lg-5 col-md-5 col-sm-auto col-auto">
                     <li>
                         <a href="
 <?php echo esc_url( home_url( '/' ) ); ?>
@@ -134,8 +134,8 @@
                         <p>Beautiful designs & Addictive ***</p>
                     </li>
                 </ul>
-                <ul class="col-xl col-lg col-md col-sm-0"></ul>
-                <ul class="col-xl-auto col-lg-auto col-md-auto col-sm-auto">
+                <ul class="col-xl col-lg col-md col-sm-auto col-auto"></ul>
+                <ul class="col-xl-auto col-lg-auto col-md-auto col-sm-auto col-auto">
                     <?php wp_nav_menu(array('menu'=>'menu')) ?>
                 </ul>
             </div>
--- inc/banner.php
+++ inc/banner.php
@@ -3,7 +3,7 @@
         <li>
             <img src="<?php echo bloginfo('template_directory'); ?>/img/1.jpg" alt="">
             <div class="container">
-                <div class="row">
+                <div class="row muhi">
                     <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
                         <span>i love new york!</span>
                     </div>
@@ -14,14 +14,14 @@
 </div>
 <div class="tm2">
     <div class="container">
-        <div class="row">
-            <div class="col-xl-7 col-lg-7 col-md-8 col-sm-8">
+        <div class="row justify-content-sm-center">
+            <div class="col-xl-7 col-lg-7 col-md-8 col-sm col">
                 <div class="tn1">
                     Welcome to Tee Molly, we bring you the great design! You have a good quote, send it to us, we will design it for you and you just buy it.
                 </div>
             </div>
-            <div class="col-xl col-lg col-md col-sm"></div>
-            <div class="col-xl-auto col-lg-auto col-md-auto col-sm-auto">
+            <div class="col-xl col-lg col-md col-sm-0 col-0"></div>
+            <div class="col-xl-auto col-lg-auto col-md-auto col-sm-auto col">
                 <div class="tn2">
                     <a href="<?php echo esc_url( home_url( '/' ) ); ?>/?page_id=14">Sent Quotes</a>
                 </div>
